e572be84682add56fb2f3a075f73376765a63fd0
KJay221 <k20002216408@gmail.com>
2022-05-30 16:52:58 +0800

    test linux gcd

--- quiz2/q3_3.c
+++ quiz2/q3_3.c
@@ -3,22 +3,67 @@
 #include <stdlib.h>
 #include <time.h>
 
-uint64_t gcd64(uint64_t u, uint64_t v)
+#define swap(a, b) \
+    do { \
+        a ^= b; \
+        b ^= a; \
+        a ^= b; \
+    } while (0)
+
+unsigned long gcd1(unsigned long a, unsigned long b)
 {
-    if (!u || !v) return u | v;
-    int shift = __builtin_ctz(u) ^ ((__builtin_ctz(u) ^ __builtin_ctz(v)) & -(__builtin_ctz(u) > __builtin_ctz(v)));
-    u >>= __builtin_ctz(u);
-    do {
-        v >>= __builtin_ctz(v);
-        if (u < v) {
-            v -= u;
-        } else {
-            uint64_t t = u - v;
-            u = v;
-            v = t;
-        }
-    } while (v);
-    return u << shift;
+	unsigned long r = a | b;
+
+	if (!a || !b)
+		return r;
+	r &= -r;
+
+	while (!(b & r))
+		b >>= 1;
+	if (b == r)
+		return r;
+
+	for (;;) {
+		while (!(a & r))
+			a >>= 1;
+		if (a == r)
+			return r;
+		if (a == b)
+			return a;
+		if (a < b)
+            swap(a, b);		
+		a -= b;
+		a >>= 1;
+		if (a & r)
+			a += b;
+		a >>= 1;
+    }
+}
+
+unsigned long gcd2(unsigned long a, unsigned long b)
+{
+	unsigned long r = a | b;
+
+	if (!a || !b)
+		return r;
+	r &= -r;
+
+	while (!(b & r))
+		b >>= 1;
+	if (b == r)
+		return r;
+
+	for (;;) {
+		while (!(a & r))
+			a >>= 1;
+		if (a == r)
+			return r;
+		if (a == b)
+			return a;
+		if (a < b)
+            swap(a, b);
+        a -= b;		
+    }
 }
 
 int main(){
@@ -26,9 +71,23 @@ int main(){
     uint64_t data[200000] = {0};
     for(int i = 0; i<200000; i++)
         data[i] = rand();
+    
+    clock_t start_t,finish_t;
+    double total_t = 0;
 
+    start_t = clock();
     for(int i = 0;i < 200000;i += 2)
-        gcd64(data[i],data[i+1]);
+        gcd1(data[i],data[i+1]);
+    finish_t = clock();
+    total_t = (double)(finish_t - start_t) / CLOCKS_PER_SEC;
+    printf("time: %f s\n", total_t);
+
+    start_t = clock();
+    for(int i = 0;i < 200000;i += 2)
+        gcd2(data[i],data[i+1]);
+    finish_t = clock();
+    total_t = (double)(finish_t - start_t) / CLOCKS_PER_SEC;
+    printf("time: %f s\n", total_t);
 
     return 0;
 }
